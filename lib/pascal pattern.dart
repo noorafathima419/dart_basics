a87bd39f7beab69707491cd5a440a22168a89f66
HP <noorafathimafeb26@gmail.com>
2024-07-26 11:18:08 +0530

    25/07/2024

--- lib/pascal pattern.dart
+++ lib/pascal pattern.dart
@@ -5,3 +5,17 @@ int  i, j;
 print("enter the number of rows");
 var a = stdin.readLineSync();
 int n = int.parse(a!);
+for(i=0;i<=1;i++)
+ {
+   for (j = 0; j <= i - 1; j++)
+   {
+     stdout.write(" ");
+   }
+
+   for (j = 0; j <= i; j++)
+   {
+     stdout.write("*");
+   }
+   stdout.write("\n");
+ }
+}
