3f681d6238c8722d0ad7c99e4ee70d1ecb5516ca
HP <noorafathimafeb26@gmail.com>
2024-08-06 11:56:43 +0530

    05/08/2024

--- lib/class and object.dart
+++ lib/class and object.dart
@@ -1,38 +1,55 @@
 
-
+//
+// void main()
+// {
+// Student noora=Student("noora","koyilandy",20);//create instance object
+// Student naja=Student("naja","karuvannur", 21);
+// Student anjana=Student("anjana","kovoor",19);
+// // print(a.name);
+// // print(a.age);
+// noora.details();
+// naja.details();
+// anjana.details();
+// }
+//
+//
+// class Student //
+// {
+// // String name="noora";
+// // String age="20";
+// String name;
+// String place;
+// int age;
+//
+//
+// Student (this.name,this.place,this.age);//paramiterised  constructor
+// details()
+// // Student()//defualt constructor
+// {
+//   // print("Hi Developers");
+//   // int a=10;
+//   // int b=10;
+//   // int sum=a+b;
+//   // print("sum is:$sum");
+//   //  print(name);
+//   //  print(place);
+//   //  print(age);
+// }
+// }
 void main()
 {
-Student noora=Student("noora","koyilandy",20);//create instance object
-Student naja=Student("naja","karuvannur", 21);
-Student anjana=Student("anjana","kovoor",19);
-// print(a.name);
-// print(a.age);
-noora.details();
-naja.details();
-anjana.details();
+  Student noora=Student(name: "noora", place:"koyilandy");
+  noora.details();
 }
 
-
-class Student //
+class Student
 {
-// String name="noora";
-// String age="20";
-String name;
-String place;
-int age;
-
-
-Student (this.name,this.place,this.age);//paramiter using constructor
-details()
-// Student()
-{
-  // print("Hi Developers");//default constructor
-  // int a=10;
-  // int b=10;
-  // int sum=a+b;
-  // print("sum is:$sum");
-   print(name);
-   print(place);
-   print(age);
-}
+  String name;
+  String place;
+  Student({required this.name,required this.place});//named constructor
+  details()
+  {
+    print(name);
+    print(place);
+  }
 }
